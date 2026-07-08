346b2600a950db3bdcf9d42b9b807406d7f4913c
amirachi388-glitch <amirachi388@gmail.com>
2026-07-08 08:26:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,11 @@ import queue
 import threading
 import time
 import psutil
-#import whisper
 from telebot import types
 from moviepy import VideoFileClip, AudioFileClip
 
-BOT_TOKEN = "8817595725:AAHJaK-hl87XPh51ToF_20oTIQ_nkzV2mgQ"
-ADMIN_ID = 8084323446  
+BOT_TOKEN = "8817595725:AAHJaK-h187XPh51ToF_20oTIQ_nkzV2mgQ"
+ADMIN_ID = 8084323446
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
